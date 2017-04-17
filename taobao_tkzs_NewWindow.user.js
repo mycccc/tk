91e28c3acac20906085f3d1642f96ca9af4782e7
mycccc <mycccc@users.noreply.github.com>
2017-04-17 21:40:47 +0800

    Create  taobao_tkzs_NewWindow.user.js

---
+++ taobao_tkzs_NewWindow.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         taobao_tkzs_NewWindow
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://detail.tmall.com/item.htm*
+// @include     https://item.taobao.com/item.htm*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    window.open('http://cha.taokezhushou.com/?url=' +encodeURIComponent(document.location.href),'getA','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=yes, width=930, height=600');void(0);
+})();
