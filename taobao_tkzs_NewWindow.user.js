b57c4a1a151301e113817e8c1c052006c4fe3bb9
mycccc <mycccc@users.noreply.github.com>
2017-04-20 09:04:25 +0800

    Update taobao_tkzs_NewWindow.user.js

--- taobao_tkzs_NewWindow.user.js
+++ taobao_tkzs_NewWindow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         taobao_tkzs_NewWindow
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://detail.tmall.com/item.htm*
@@ -13,5 +13,6 @@
     'use strict';
 
     // Your code here...
-    window.open('http://cha.taokezhushou.com/?url=' +encodeURIComponent(document.location.href),'getA','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=yes, width=930, height=600');void(0);
+    //window.open('http://cha.taokezhushou.com/?url=' +encodeURIComponent(document.location.href),'getA','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=yes, width=930, height=600');void(0);
+    window.open('http://cha.taokezhushou.com/?url=' +encodeURIComponent(document.location.href));
 })();
