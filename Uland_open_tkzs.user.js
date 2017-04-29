41bc22387bce8e5d7f9e1114252aa417d1e3547a
mycccc <mycccc@users.noreply.github.com>
2017-04-29 09:43:57 +0800

    Update Uland_open_tkzs.user.js

--- Uland_open_tkzs.user.js
+++ Uland_open_tkzs.user.js
@@ -4,7 +4,7 @@
 // @version      0.3
 // @description  try to take over the world!
 // @author       You
-// @match        http://uland.taobao.com/coupon/edetail*
+// @match        http*://uland.taobao.com/coupon/edetail*
 // @grant        none
 // ==/UserScript==
 
